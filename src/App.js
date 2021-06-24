0932d216d4afc8ec5293598ad07701b5bf933cd8
ryanspivey <rlspivey@go.olemiss.edu>
2021-06-24 13:58:59 -0500

    navbar links hide based on login status

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 // import "./styles.css";
 import "./Styles/HomeStyles.css";
-import { Route, Switch, Link } from "react-router-dom";
-// import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 
 // IMPORT COMPONENTS
 import Navbar from './components/Navbar';
@@ -12,17 +11,28 @@ import PrivateRoute from "./components/Authentication/PrivateRoute";
 import Home from "./components/Home";
 
 function App(props) {
- 
+  const [isLogin, setIsLogin] = useState();
+  
+  const checkForToken = () => {
+    if(window.localStorage.getItem("token"))
+      setIsLogin(true);
+    else
+      setIsLogin(false);
+  }
 
+  useEffect(() => {
+    checkForToken();
+  }, []);
+ 
   return (
     <Fragment>
       
-      <Navbar />
+      <Navbar isLogin={isLogin} checkForToken={checkForToken} />
 
       <Switch>
-        <Route exact path="/register" component={SignUpForm} />
-        <Route exact path="/login" component={LoginForm} />
-        <Route exact path="/manager" component={PrivateRoute} />
+        <Route exact path="/register" render={() => <SignUpForm />} />
+        <Route exact path="/login" onClick={checkForToken} render={() => <LoginForm checkForToken={checkForToken}/>} />
+        <Route exact path="/manager" render={() => <PrivateRoute />} />
         <Route exact path="/" component={Home} />
       </Switch>
 
